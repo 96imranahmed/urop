2836b8f997648618b80ad30724966153c6ac6210
Imran Ahmed <96imranahmed@gmail.com>
2017-08-01 18:32:17 +0100

    Added models

--- tracker_multi.py
+++ tracker_multi.py
@@ -13,12 +13,12 @@ face_settings = { 'confidence': 5, 'orientations': [0, 0.125, 0.25, 0.375, 0.5, 
 face_suppress_settings = {'round_to_val': 30, 'radii_round': 50, 'stack_length': 5, 'positive_thresh': 4, \
  'remove_thresh': -1, 'step_add': 1, 'step_subtract': -2, 'coarse_scale': 8.0, 'coarse_radii_scale': 3.0}
 
-large_size_settings = { 'confidence': 5, 'orientations': [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875], \
-                    'scale': 1.2, 'stride':0.2, 'min_size': 70  }
+large_size_settings = { 'confidence': 7, 'orientations': [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875], \
+                    'scale': 1.3, 'stride':0.25, 'min_size': 80  }
 
 test_path = os.getcwd() + '/m_10_side_wide'
-feed_list = [ (False, 'faces', face_settings, face_suppress_settings, False, 640.0), (False, 'faces', large_size_settings, face_suppress_settings, False, 1280.0),
-(False, 'faces', face_settings, face_suppress_settings, False, 640.0)]
+feed_list = [ (False, 'faces', face_settings, face_suppress_settings, False, 640.0),
+                (False, 'faces', large_size_settings, face_suppress_settings, False, 1280.0)]
 #(False, 'faces', side_settings, face_suppress_settings, False, 640.0)
 
 #The tuple is specified as follows:
